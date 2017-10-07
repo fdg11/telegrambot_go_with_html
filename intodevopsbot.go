909be74d9aaaa36fba3cafc208bcf5c878466673
Alex P <alexfile123@gmail.com>
2017-10-07 16:47:52 +0300

    Update intodevops

--- intodevopsbot.go
+++ intodevopsbot.go
@@ -12,24 +12,24 @@ import (
 )
 
 var (
-	// глобальная переменная в которой храним токен и chatID
-	telegramBotToken string
-	chatID int64
-	sliceMsg map[string]string = make(map[string]string)
+	telegramBotToken string // токен
+	chatID int64 // чат id
+	sliceMsg map[string]string = make(map[string]string) // карта полей формы
 )
 
 func init() {
-	// принимаем на входе флаг -telegrambottoken и chatid
+	// принимаем на входе флаги -telegrambottoken и chatid
 	flag.StringVar(&telegramBotToken, "telegrambottoken", "", "Telegram Bot Token")
 	flag.Int64Var(&chatID, "chatid", 0, "chatId to send messages")
 	flag.Parse()
 
-	// без него не запускаемся
+	// без токена не запускаемся
 	if telegramBotToken == "" {
 		log.Print("-telegrambottoken is required")
 		os.Exit(1)
 	}
 
+	// без чат id не запускаемся
 	if chatID == 0 {
 		log.Print("-chatid is required")
 		os.Exit(1)
@@ -38,13 +38,14 @@ func init() {
 }
 
 func client(w http.ResponseWriter, r *http.Request) {
+	// создаем шаблон
 	fp := path.Join("", "client.html")
 	tmpl, err := template.ParseFiles(fp)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	// Pass template to http.ResponseWriter.
+	// отвечаем по шаблону
 	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -52,19 +53,23 @@ func client(w http.ResponseWriter, r *http.Request) {
 }
 
 func process(w http.ResponseWriter, r *http.Request) {
+	// используя токен создаем новый инстанс бота
 	bot, err := tgbotapi.NewBotAPI(telegramBotToken)
 	if err != nil {
 		log.Panic(err)
 	}
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+
+	// Парсим содержимое формы
 	r.ParseForm()
+	// Пишем лог парсинга формы
 	log.Printf("Test name: %s", r.PostForm)
 
+	// Получаем данные из формы
 	sliceMsg["name"] = r.PostFormValue("name")
 	sliceMsg["phone"] = r.PostFormValue("phone")
 	sliceMsg["message"] = r.PostFormValue("messages")
 
+	// Проверка на заполнение (Доработать!)
 	if sliceMsg["name"] == "" {
 		fmt.Fprintln(w, "Не заполнено поле 'Ваше Имя'")
 	} else if len(sliceMsg["phone"]) > 12 || sliceMsg["phone"] == ""  {
@@ -73,6 +78,7 @@ func process(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Не заполнено поле 'Сообщение'")
 	} else {
 		fmt.Fprintln(w, "Сообщение отправлено")
+		// Формируем текс сообщения в телеграм
 		text := fmt.Sprintf(
 			"`%s`\n" +
 				"*Имя отправителя:* _%s_\n" +
@@ -83,7 +89,9 @@ func process(w http.ResponseWriter, r *http.Request) {
 				sliceMsg["phone"],
 				sliceMsg["message"])
 		msg := tgbotapi.NewMessage(chatID, text)
+		// Парсем в "Markdown"
 		msg.ParseMode = "markdown"
+		// Отправляем
 		bot.Send(msg)
 	}
 }
@@ -95,13 +103,16 @@ func main() {
 		log.Panic(err)
 	}
 
+	// пишем лог авторизации бота
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	// u - структура с конфигом для получения апдейтов
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	// Отправляем сообщение в телеграм, что бот активен
 	bot.Send(tgbotapi.NewMessage(chatID, fmt.Sprintf("%s", "Бот запущен, ожидает сообщений с сайт")))
 
+	// Структура с конфигом для получения апдейтов
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	// Включаем сервер для теста прописываем роуты
 	server := http.Server{
 		Addr: ":8080",
 	}
@@ -112,30 +123,27 @@ func main() {
 
 	// используя конфиг u создаем канал в который будут прилетать новые сообщения
 	updates, err := bot.GetUpdatesChan(u)
-
 	// в канал updates прилетают структуры типа Update
 	// вычитываем их и обрабатываем
 	for update := range updates {
-		// универсальный ответ на любое сообщение
-		_ = "Не знаю что сказать"
+		// универсальный ответ на любое сообщение (Доработать!)
+		reply := "Не знаю что сказать"
 		if update.Message == nil {
 			continue
 		}
-
 		// логируем от кого какое сообщение пришло
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 		log.Printf("[%s] %s", update.Message.Chat.ID, update.Message.Text)
-		// свитч на обработку комманд
+		// свитч на обработку комманд (Доработать!)
 		// комманда - сообщение, начинающееся с "/"
 		switch update.Message.Command() {
 		case "start":
-			_ = "Привет. Я телеграм-бот"
+			reply = "Привет. Я телеграм-бот"
 		case "hello":
-			_ = "world"
+			reply = "world"
 		}
-
 		// создаем ответное сообщение
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 		//msg.ReplyToMessageID = update.Message.MessageID
 		// отправляем
 		bot.Send(msg)
